9d7975806c5f4eff1e23a0312a680020ec913d19
nitipbox <nitipboxaja@gmail.com>
2026-07-05 10:12:45 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+var CACHE_NAME = 'nitipbox-cache-v1';
+var URLS_TO_CACHE = ['/', '/index.html'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE_NAME; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
